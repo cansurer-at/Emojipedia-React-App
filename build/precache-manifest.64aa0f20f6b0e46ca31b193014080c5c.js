d35393043c452b806583d259d736da68b3b769e1
Can Surer <cansurer@outlook.com>
2022-02-28 14:19:53 +0300

    apply build

---
+++ build/precache-manifest.64aa0f20f6b0e46ca31b193014080c5c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4234382d588188b7b46a337334948bd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "eb24a655caad790fe164",
+    "url": "/static/js/main.d23e9a50.chunk.js"
+  },
+  {
+    "revision": "f3ab9bf2fde6b43969db",
+    "url": "/static/js/runtime-main.636b7682.js"
+  }
+]);
