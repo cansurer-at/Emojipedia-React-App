ff907d146551a5c7089561820c6348542dc67e58
Can Surer <cansurer@outlook.com>
2022-02-28 14:26:47 +0300

    Updates

---
+++ precache-manifest.7670714e426f1be0d6be6c516e08f30c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc466e738e2ffe41a5ad8421b54cd862",
+    "url": "/Emojipedia-React-App/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "/Emojipedia-React-App/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "eb24a655caad790fe164",
+    "url": "/Emojipedia-React-App/static/js/main.d23e9a50.chunk.js"
+  },
+  {
+    "revision": "8ef2ee880c7566333193",
+    "url": "/Emojipedia-React-App/static/js/runtime-main.7b48daea.js"
+  }
+]);
